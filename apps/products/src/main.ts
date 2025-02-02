62b375e16fc3e2429a8768a55820576e3839b792
Oussama Jaaouani <136766292+oussama-debug@users.noreply.github.com>
2025-02-02 15:16:14 -0500

    fix redis products

--- apps/products/src/main.ts
+++ apps/products/src/main.ts
@@ -3,17 +3,14 @@ import { Transport, type MicroserviceOptions } from '@nestjs/microservices';
 import { ProductsModule } from './products.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    ProductsModule,
-    {
-      transport: Transport.REDIS,
-      options: {
-        url: process.env.GATEWAY_BUS_URL as string,
-        retryAttempts: 5,
-        retryDelay: 1000,
-      },
+  const app = await NestFactory.createMicroservice(ProductsModule, {
+    transport: Transport.REDIS,
+    options: {
+      url: process.env.GATEWAY_BUS_URL as string,
+      retryAttempts: 5,
+      retryDelay: 1000,
     },
-  );
+  });
   await app.listen();
 }
 bootstrap();
